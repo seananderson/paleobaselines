ba8941a7e2b0117dfc0f044e9530eb7963730f0a
Sean Anderson <github@seananderson.ca>
2013-12-22 12:43:30 -0500

    Switch to rsync for mirror

--- update-files.sh
+++ update-files.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-cd r
-cp ~/Dropbox/nescent_extinction_map/r/*.R .
-cp ~/Dropbox/nescent_extinction_map/r/*.r .
-rm *conflicted\ copy*
+rsync -rav --delete --include '*.R$' --include '*.r$' --exclude '*' ~/Dropbox/nescent_extinction_map/r/ r/
+#rm r/*conflicted\ copy*
