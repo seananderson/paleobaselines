76bcfd004a1912fc866bce13c8fcfd1def197575
Sean Anderson <github@seananderson.ca>
2014-01-29 13:00:14 -0800

    Update package functions to match new SOM

--- paleorisk/R/fit_interval_gbm.R
+++ paleorisk/R/fit_interval_gbm.R
@@ -17,20 +17,33 @@
 #' @export
 
 fit_interval_gbm <- function(dat, stage_train, stage_test, interaction.depth = 1,
-  n.trees = 300, shrinkage = 0.1, ...) {
+  n.trees = 300, shrinkage = 0.1, use_weights = FALSE, ...) {
   message(paste("Predicting", stage_test, "from", stage_train))
   dat_train <- dat[dat$stage == stage_train, ]
   dat_test  <- dat[dat$stage == stage_test,  ]
+  
+  if(use_weights) {   
+    w <- get_weights(dat_train$Ex)
+    weights_train <- ifelse(dat_train$Ex == 1, w$ex_weight, w$sur_weight)
+    
+    w <- get_weights(dat_test$Ex)
+    weights_test <- ifelse(dat_test$Ex == 1, w$ex_weight, w$sur_weight)
+    
+  } else {
+    weights_train <- rep(1, nrow(dat_train))
+    weights_test <- rep(1, nrow(dat_test))
+  }
+  
   # cross-stage model:
-  m_test <- gbm(Ex ~ richness + occupancy + occurrences + min.lat +
+  m_test <- gbm::gbm(Ex ~ richness + occupancy + occurrences + min.lat +
       max.lat + lat.range + mean.lat + great.circle + class + group,
     data = dat_train, n.trees = n.trees, interaction.depth = interaction.depth,
-    distribution = "bernoulli", shrinkage = shrinkage, ...)
+    distribution = "bernoulli", shrinkage = shrinkage, weights = weights_train, ...)
   # test stage self model:
   m_self <- gbm::gbm(Ex ~ richness + occupancy + occurrences + min.lat +
       max.lat + lat.range + mean.lat + great.circle + class + group,
     data = dat_test, n.trees = n.trees, interaction.depth = interaction.depth,
-    distribution = "bernoulli", shrinkage = shrinkage, ...)
+    distribution = "bernoulli", shrinkage = shrinkage, weights = weights_test, ...)
   pred <- gbm::predict.gbm(m_test, newdata = dat_test, type = "response",
     n.trees = n.trees)
   pred_self <- gbm::predict.gbm(m_self, type = "response", n.trees = n.trees)
--- paleorisk/R/validate_gbm.R
+++ paleorisk/R/validate_gbm.R
@@ -32,6 +32,7 @@
 #' 
 #' If \code{classification_stats = FALSE} then the list will only have the 
 #' prediction and bin validation data frames.
+#' @rdname validate_gbm
 
 validate_gbm <- function(dat, test_fraction = 0.5, threshold = 0.5,
   shrinkage = 0.1, interaction.depth = 1, n.trees = 300, bin = 0.025, 
@@ -58,31 +59,18 @@ validate_gbm <- function(dat, test_fraction = 0.5, threshold = 0.5,
   # just in case these already exist:
   dat_test$gbm_pred <- NULL
   
-  if(use_weights) {    
-    obs <- nrow(dat_train)
-    exes <- sum(dat_train$Ex)
-    ExFreq <- (exes/obs)
-    SurFreq <- (1-(exes/obs))
-    MaxFreq <- max(ExFreq,SurFreq)
-    ExWeight <- (1/(ExFreq/MaxFreq))
-    SurWeight <- (1/(SurFreq/MaxFreq))
-    weights <- ifelse(dat_train$Ex==1, ExWeight, SurWeight)
-  } else {
-    weights <- rep(1, nrow(dat_train))
-  }
+   if(use_weights) {   
+     w <- get_weights(dat_train$Ex)
+     weights <- ifelse(dat_train$Ex == 1, w$ex_weight, w$sur_weight)
+   } else {
+     weights <- rep(1, nrow(dat_train))
+   }
   
   # model fitting:
-  if(use_weights) {
   m <- gbm::gbm(Ex ~ richness + occupancy + occurrences + min.lat + max.lat +
       lat.range + mean.lat + great.circle + class + group, data =
       dat_train, n.trees = n.trees, interaction.depth = interaction.depth,
     distribution = "bernoulli", shrinkage = shrinkage, weights = weights, ...)
-  } else {
-    m <- gbm::gbm(Ex ~ richness + occupancy + occurrences + min.lat + max.lat +
-        lat.range + mean.lat + great.circle + class + group, data =
-        dat_train, n.trees = n.trees, interaction.depth = interaction.depth,
-      distribution = "bernoulli", shrinkage = shrinkage, ...)
-  }
   
   dat_train$gbm_pred <- gbm::predict.gbm(m, n.trees = n.trees, type = "response")
   dat_test$gbm_pred <- gbm::predict.gbm(m, n.trees = n.trees, type = "response",
@@ -107,4 +95,53 @@ validate_gbm <- function(dat, test_fraction = 0.5, threshold = 0.5,
   } else {
     return(list(pred = dat_test, bin_validation = bin_validation))
   }
+}
+
+#' Get model weights
+#'
+#' @param x A vector of 0s and 1s with 0s representing non-extinctions and 1s
+#'   representing extinctions.
+#' @return A list with extiction and survival weights to be passed to a model.
+#' @examples
+#' get_weights(c(0, 0, 1))
+#' @export
+#' @rdname validate_gbm
+
+get_weights <- function(x) {
+  obs <- length(x)
+  exes <- sum(x)
+  ExFreq <- (exes/obs)
+  SurFreq <- (1-(exes/obs))
+  MaxFreq <- max(ExFreq,SurFreq)
+  ExWeight <- (1/(ExFreq/MaxFreq))
+  SurWeight <- (1/(SurFreq/MaxFreq))
+  list(ex_weight = ExWeight, sur_weight = SurWeight)
+} 
+
+#' SMOTE our data
+#' @rdname validate_gbm
+
+smote <- function(x, perc.over = 800, perc.under = 100) {
+  x$Ex <- as.factor(x$Ex)
+  x <- DMwR::SMOTE(Ex ~ ., data = x[,c("class", "group", 
+    "great.circle", "mean.lat", "lat.range", "max.lat", "min.lat", 
+    "occurrences", "occupancy", "richness", "Ex")], 
+    perc.over = perc.over, perc.under = perc.under)
+  x$Ex <- as.numeric(as.character(x$Ex))
+  x
+}
+
+#' Summarize a validation test
+#'
+#' @export
+#' @rdname validate_gbm
+
+summarize_val_test <- function(x) {
+  m <- stats::glm(obs_ext_prob ~ 1, family = binomial, data = x,
+    weights = sample_n)
+  int <- boot::inv.logit(coef(m)[[1]])
+  cis <- boot::inv.logit(confint(m))
+  median_sample_n <- median(x$sample_n)
+  data.frame(mean_observed = int, l = cis[[1]], u = cis[[2]],
+    median_sample_n = median_sample_n)
 }
