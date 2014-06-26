18174c0f1d732824d086089d7dc7eacc18a9eb46
Sean Anderson <github@seananderson.ca>
2014-06-26 11:27:01 -0700

    Revert to mix of old and new standardization scripts

--- paleorisk/R/standardize_data.R
+++ paleorisk/R/standardize_data.R
@@ -1,6 +1,11 @@
 #' Standardize paleontological and contemporary datasets
 #'
 #' @param dat The data frame to standardize.
+#' @param min_lat_bin TODO
+#' @param max_lat_bin TODO
+#' @param lat_range_bin TODO
+#' @param mean_lat_bin TODO
+#' @param tropical_only_bin
 #' @param great_circle_bin
 #' @param num_bins Number of bins to use for standardizing richness,
 #' occurrences, occupancy.
@@ -19,65 +24,53 @@
 #'
 #' @export
 
-standardize_data <- function(dat, interval, great_circle_bin = 2000,
-  num_bins = 10, min_modern_occurrences = 2, min_pbdb_occurrences = 1,
-  modern_no_cull_classes = c("Mammalia", "Elasmobranchii")) {
+standardize_data <- function(dat, min_lat_bin = 10, max_lat_bin = 10,
+  lat_range_bin = 10, mean_lat_bin = 10,
+  tropical_only_bin = 1, great_circle_bin = 2000, num_bins = 10,
+  min_modern_occurrences = 2) {
 
-  mod.dat <- dat[(dat$stage_top == 0),]
-  fossil.dat <- dat[(dat$stage_top > 0),]
-
-  mod.dat.cull <- mod.dat[!mod.dat$class %in% modern_no_cull_classes, ]
-  mod.dat.no.cull <- mod.dat[mod.dat$class %in% modern_no_cull_classes, ]
-
-  mod.dat.cull <- mod.dat.cull[(mod.dat.cull$occurrences >= min_modern_occurrences), ]
-  mod.dat <- rbind(mod.dat.cull, mod.dat.no.cull)
-
-  fossil.dat <- fossil.dat[(fossil.dat$occurrences >= min_pbdb_occurrences),]
-
-  data1 <- rbind(mod.dat,fossil.dat)
-
-  data1 <- subset(data1, Interval_Name == interval)
+  if(unique(dat$Interval_Name[1]) %in% c("Modern_merged", "Modern_raw")) {
+    data1  <- drop.levels(subset(dat, dat$occurrences >= min_modern_occurrences))
+  } else {
+    data1  <- droplevels(dat)
+  }
 
   stage_name <- data1$Interval_Name[1]
   stage_top <- data1$stage_top[1]
 
-  ### create factors to analyze from prediction interval
+  ### create factors to analyze from prediction interval, round if desired
   genus <- data1$genus
   class <- data1$class
   group <- data1$MatchTaxon
 
   max.rich <- max(log(data1$richness))
-  richness <- round((log(data1$richness)/max.rich),1)
+  richness <- round(log(data1$richness)/max.rich, 1)
 
   max.occupancy <- max(log(data1$eac))
-  occupancy <- round((log(data1$eac)/max.occupancy ),1)
+  occupancy <- round(log(data1$eac)/max.occupancy, 1)
 
   max.occurrences <- max(log(data1$occurrences))
-  occurrences <- round((log(data1$occurrences)/max.occurrences ),1)
+  occurrences <- round(log(data1$occurrences)/max.occurrences, 1)
 
   gcd <- data1$gcd
-  great.circle <- round((gcd/great_circle_bin),0)*great_circle_bin
+  great.circle <- round(gcd/great_circle_bin)*great_circle_bin
   lats <- data.frame(abs(data1$MinLat), abs(data1$MaxLat))
 
-  #paleolats & longs
-  min.lat <- apply(lats, 1, min)
-  max.lat <- apply(lats, 1, max)
+  #paleolats & longs rounded
+  min.lat <- floor(apply(lats, 1, min)/min_lat_bin)*min_lat_bin
+  max.lat <- ceiling(apply(lats, 1, max)/max_lat_bin)*max_lat_bin
   lat.range <- max.lat-min.lat
-  mean.lat <- abs(data1$mean_lat)
-
-  min.lat <- floor(min.lat / num_bins)*num_bins
-  max.lat <- ceiling(max.lat / num_bins)*num_bins
-  lat.range <- round(lat.range/num_bins)*num_bins
-  mean.lat <- round(mean.lat/num_bins)*num_bins
+  mean.lat <- round(abs(data1$mean_lat)/mean_lat_bin)*mean_lat_bin
+  tropical_only <- ifelse(max.lat > 30, 0, 1)
 
   Ex <- data1$Extinct.in.stage
 
   stage <- rep(stage_name, length(Ex))
   stage_top <- rep(stage_top, length(Ex))
 
-  stand_dat <- gdata::drop.levels(na.omit(data.frame(stage, stage_top, class, group,
+  stand_dat <- droplevels(na.omit(data.frame(stage, stage_top, class, group,
         genus, richness, occupancy, occurrences, min.lat, max.lat, lat.range,
-        mean.lat, great.circle, Ex)))
+        mean.lat, great.circle, tropical_only, Ex)))
 
   return(stand_dat)
 }
