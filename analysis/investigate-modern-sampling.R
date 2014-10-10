d173510ef6585902c58b575740de956f709acb6e
Sean Anderson <github@seananderson.ca>
2014-10-10 12:33:39 -0700

    Tweak modern sampling sensitivity code

--- analysis/investigate-modern-sampling.R
+++ analysis/investigate-modern-sampling.R
@@ -17,31 +17,31 @@ ggsave("meanext-vs-obis-n-by-prov.pdf", width = 6, height = 5)
 # ...
 # starting again:
 
-load("../data/composite.occ2.rda")
-library(dplyr)
-counts <- composite.occ2 %>% group_by(genus) %>%
-  summarise(n = n())
-modern <- left_join(modern, counts)
+#load("../data/composite.occ2.rda")
+#library(dplyr)
+#counts <- composite.occ2 %>% group_by(genus) %>%
+  #summarise(n = n())
+#modern <- left_join(modern, counts)
 
 modern_long <- reshape2::melt(modern, id.vars = c("class", "group", "genus"),
   measure.vars = c("richness", "occupancy", "min.lat", "max.lat", "lat.range",
-    "mean.lat", "great.circle", "gbm_pred_binned"))
-modern_long <- inner_join(modern_long, modern[,c("genus", "n")])
+    "mean.lat", "great.circle", "mean_observed"))
+modern_long <- inner_join(modern_long, modern[,c("genus", "obis_n")])
 modern_long <- filter(modern_long, !class %in% c("Mammalia", "Elasmobranchii"))
 
- p1 <- ggplot(filter(modern_long, variable %in% c("richness", "occupancy")), aes(log10(n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 0.1), alpha = 0.15)
+ p1 <- ggplot(filter(modern_long, variable %in% c("richness", "occupancy")), aes(log10(obis_n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 0.1), alpha = 0.15)
 
-p4 <- ggplot(filter(modern_long, variable %in% c("gbm_pred_binned")), aes(log10(n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 0.05), alpha = 0.2)
+p4 <- ggplot(filter(modern_long, variable %in% c("mean_observed")), aes(log10(obis_n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 0.05), alpha = 0.2)
 
-p2 <- ggplot(filter(modern_long, variable %in% c("min.lat", "max.lat", "lat.range", "mean.lat")), aes(log10(n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 3), alpha = 0.1)
+p2 <- ggplot(filter(modern_long, variable %in% c("min.lat", "max.lat", "lat.range", "mean.lat")), aes(log10(obis_n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 3), alpha = 0.1)
 
-p3 <- ggplot(filter(modern_long, variable %in% c("great.circle")), aes(log10(n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 600), alpha = 0.2)
+p3 <- ggplot(filter(modern_long, variable %in% c("great.circle")), aes(log10(obis_n), value, colour = class)) + facet_wrap(~variable) + geom_point(position = position_jitter(height = 600), alpha = 0.2)
 
 pdf("obis-samping-vs-predictors.pdf", width = 10, height = 7)
 gridExtra::grid.arrange(p4, p3, p1, p2)
 dev.off()
 
-p <- ggplot(modern_long, aes(value, log10(n), colour = class)) + facet_grid(class~variable, scales = "free_x") + geom_point(position = position_jitter(height = 0.1), alpha = 0.09)
+p <- ggplot(modern_long, aes(value, log10(obis_n), colour = class)) + facet_grid(class~variable, scales = "free_x") + geom_point(position = position_jitter(height = 0.1), alpha = 0.09)
 ggsave("obis-sampling-vs-predictors-class-facets.pdf", width = 14, height = 7)
 
 # `🐳` <- "whale!"
