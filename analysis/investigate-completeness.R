586157c6776f17e4b2e3a91fc3f96560e55a39c4
Sean Anderson <github@seananderson.ca>
2014-12-12 10:32:30 -0800

    Move Neogene preserv. prob. histogram fig

--- analysis/investigate-completeness.R
+++ analysis/investigate-completeness.R
@@ -8,9 +8,6 @@
 neog <- readRDS("../data/stand-predictors-cen-obis.rds")
 neog <- droplevels(subset(neog, stage_top < 23 & stage_top != 0))
 
-p1 <- ggplot(neog, aes(prop_comp, fill = class)) + geom_histogram() + xlab("Preservation probability") + ylab("Number of genera") + labs(fill = "Group")
-ggsave("../figs/hist-preserv-prob.pdf", width = 6, height = 4)
-
 ne2 <- neog
 ne2_no_singles <- subset(ne2, single_obs == 0)
 ne2$single_obs_TF <- FALSE
