bf19dd22f4b453b94aedb9d114b0164029bfa6c0
Sean Anderson <github@seananderson.ca>
2014-12-19 10:49:30 -0800

    Add Seth's changes to risk-pres-prob-scatter.R

--- analysis/risk-pres-prob-scatter.R
+++ analysis/risk-pres-prob-scatter.R
@@ -1,5 +1,3 @@
-#library(gridExtra)
-
 ### make data frame that includes both measures of completeness
 dat <- readRDS("../data/modern_and_paleo_ranges.rds")
 pseudo.ex <- read.csv("../data/4.6.2014.false.extinctions.csv", header = TRUE)
@@ -14,13 +12,14 @@ mean.pseudo.ex <-function(df) mean(df$FALSE.EXT)
 preservation <- plyr::ddply(dat3, plyr::.(class, group), plyr::each(mean.prop, mean.pseudo.ex))
 
 ### extract mean completness metrics
-predictions <- readRDS("../data/modern-predictions.rds")
-predictions <- predictions[(predictions$Interval_Name == "Modern_merged"), ]
+### extract mean extinction risk estimates
+cross_pred <- readRDS("../data/cross_pred.rds")
+cross_pred <- cross_pred[(cross_pred$stage_train == cross_pred$stage_test),]
 
 ### get mean values
 mean.risk <- function(df) mean(df$pred)
 n.gen <- function(df) length(df$pred)
-preds <- plyr::ddply(predictions, plyr::.(class, group), plyr::each(mean.risk, n.gen))
+preds <- plyr::ddply(cross_pred, plyr::.(class, group), plyr::each(mean.risk, n.gen))
 
 ### merge preservation and prediction
 plot.dat <- merge(preservation, preds)
@@ -28,17 +27,10 @@ plot.dat <- merge(preservation, preds)
 plot.dat$group <- data.frame(t(do.call("cbind",
       strsplit(as.character(plot.dat$group), "_"))))[, 2]
 
-p1 <- ggplot(plot.dat, aes(mean.prop, mean.risk, colour = class, label = group)) +
-  geom_point(size = 3) + geom_text(size = 3, hjust = .5,  vjust = -.6) + theme_bw() +
-  xlab("Mean preservation probability") + ylab("Mean predicted intrinsic risk") +
-  coord_cartesian(xlim = c(.65, 1)) + labs(colour = "Group")
-  #+ theme(legend.position = "none")
+p1 <- ggplot(plot.dat,aes(mean.prop,mean.risk,colour=class,label=group)) + geom_point(size=3) + geom_text(size=3,hjust=.5, vjust=-.6) + theme_bw() + xlab("mean preservation probability") + ylab("mean extinction risk prediction") + coord_cartesian(xlim=c(.65,1.03))
+# + theme(legend.position="none")
 
-p2 <- ggplot(plot.dat, aes(mean.pseudo.ex, mean.risk, colour = class, label = group)) +
-  geom_point(size = 3) + geom_text(size = 3, hjust = .5,  vjust = -.6) + theme_bw() +
-  xlab("Proportion of false extinctions") +
-  ylab("Mean predicted intrinsic risk") +
-  coord_cartesian(xlim = c(-.08, .5)) + labs(colour = "Group")
+p2 <- ggplot(plot.dat,aes(mean.pseudo.ex,mean.risk,colour=class,label=group)) +  geom_point(size=3) + geom_text(size=3,hjust=.5, vjust=-.6) + theme_bw() + xlab("Proportion of false extinctions") + ylab("mean extinction risk prediction") + coord_cartesian(xlim=c(-.08,.5))+ theme(legend.title=element_blank())
 
 pdf("../figs/risk-vs-preservation.pdf",  width = 12,  height = 5.1)
 gridExtra::grid.arrange(p1, p2, ncol = 2)
